db1af806836530075789a1487fcca35cc8377870
Ahmed Al-Thmali <althumali2006@gmail.com>
2025-04-14 11:52:03 +0300

    listview3

--- MauiApp1/ListViews.xaml.cs
+++ MauiApp1/ListViews.xaml.cs
@@ -1,3 +1,4 @@
+using MauiApp1.Models;
 namespace MauiApp1;
 
 public partial class ListViews : ContentPage
@@ -5,5 +6,13 @@ public partial class ListViews : ContentPage
 	public ListViews()
 	{
 		InitializeComponent();
-	}
+		List<UserModel> users = new List<UserModel>
+        {
+            new UserModel { Name = "John Doe", Details = "Details about John" , Img="nora.png"},
+            new UserModel { Name = "Jane Smith", Details = "Details about Jane" , Img="dotnet_bot.png"},
+            new UserModel { Name = "Sam Brown", Details = "Details about Sam" , Img="nora.png"}
+        };
+        ListView1.ItemsSource = users;
+        ListView2.ItemsSource = users;
+    }
 }
