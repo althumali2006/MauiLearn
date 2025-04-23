65a9512aac400204e4f5bf97e30d0290100a3403
Ahmed AlThmali <althumali2006@gmail.com>
2025-04-23 21:25:14 +0300

    new proj Console

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Hi App";
+            Console.WriteLine(Console.Title);
+            //Console.WriteLine(Console.ReadLine());
+            Console.WriteLine(89 + 89);
+            //Variables
+            string name = "Ahmed";
+            Console.WriteLine("Name " + name);
+            int age = 43;
+            Console.WriteLine("Age " + age);
+            int x = 10;
+            int y = 15;
+            int z;
+            z = x + y;
+            Console.WriteLine($"Z/2: {z/2}");
+            string sx = "10";
+            string sy = "15";
+            string sz = sx + sy;
+            Console.WriteLine($"SZ {sz}");
+            double dx = 10;
+            double dy = 15;
+            double dz = dx + dy;
+            Console.WriteLine($"DZ/2 {dz/2}");
+            float fx = 10;
+            float fy = 15;
+            float fz = fx + fy;
+            Console.WriteLine($"FZ/2 {fz/2}");
+        }
+    }
+}
