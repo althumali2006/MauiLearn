3f0b9fbdccd80a728ef5be2060575b9568d1ea22
Ahmed AlThmali <althumali2006@gmail.com>
2025-05-02 22:06:38 +0300

    tts3

--- TTSOffLine/MainPage.xaml.cs
+++ TTSOffLine/MainPage.xaml.cs
@@ -9,10 +9,18 @@
             TextToSpeech.Default.SpeakAsync("Hello World", new SpeechOptions { Pitch = 0, Volume = 1 }, CancellationToken.None);
         }
 
-        private void BtnSpeak_Clicked(object sender, EventArgs e)
+        private async void BtnSpeak_Clicked(object sender, EventArgs e)
         {
-            TextToSpeech.Default.SpeakAsync(WordEntry.Text, new SpeechOptions { Pitch = 0, Volume = 1 }, CancellationToken.None);
+            var locales = await TextToSpeech.Default.GetLocalesAsync();
+            var options = new SpeechOptions
+            {
+                Pitch = 1f,              // 0.0–2.0
+                Volume = 1f,             // 0.0–1.0
+                Locale = locales.FirstOrDefault(l => l.Language == "en-US")
+            };
+            await TextToSpeech.Default.SpeakAsync(WordEntry.Text, options);
+
+            //TextToSpeech.Default.SpeakAsync(WordEntry.Text, new SpeechOptions { Pitch = 1, Volume = 1 }, CancellationToken.None);
         }
     }
-
 }
