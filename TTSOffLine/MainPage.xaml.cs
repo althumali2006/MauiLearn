8bfd2856624792627d2b7be4208306ebb83d6e64
Ahmed AlThmali <althumali2006@gmail.com>
2025-05-02 21:48:12 +0300

    TTS1

--- TTSOffLine/MainPage.xaml.cs
+++ TTSOffLine/MainPage.xaml.cs
@@ -6,8 +6,13 @@
         public MainPage()
         {
             InitializeComponent();
+            TextToSpeech.Default.SpeakAsync("Hello World", new SpeechOptions { Pitch = 0, Volume = 1 }, CancellationToken.None);
         }
 
+        private void BtnSpeak_Clicked(object sender, EventArgs e)
+        {
+            TextToSpeech.Default.SpeakAsync(WordEntry.Text, new SpeechOptions { Pitch = 0, Volume = 1 }, CancellationToken.None);
+        }
     }
 
 }
