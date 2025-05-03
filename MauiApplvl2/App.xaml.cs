9982ac70fc931fe94ede3fa375b016c733605766
Ahmed AlThmali <althumali2006@gmail.com>
2025-05-03 16:27:07 +0300

    new app for db

---
+++ MauiApplvl2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApplvl2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
