a310b0f13b49b95b77aac3dd3f47a83ea96376f0
Ahmed AlThmali <althumali2006@gmail.com>
2025-05-03 08:36:56 +0300

    enhance tts speak

--- TTSOffLine/DictionaryPage.xaml.cs
+++ TTSOffLine/DictionaryPage.xaml.cs
@@ -16,8 +16,8 @@ public partial class DictionaryPage : ContentPage
     {
         var locales = TextToSpeech.Default.GetLocalesAsync().Result;
         var word = e.SelectedItem as TTSOffLine.Models.WordModel;
-        TextToSpeech.Default.SpeakAsync($"In Arabic.", new SpeechOptions { Pitch = 0f, Volume = 1f , Locale= locales.FirstOrDefault(l => l.Language == "en-IE") });
+        TextToSpeech.Default.SpeakAsync($"In Arabic.", new SpeechOptions { Pitch = 0f, Volume = 1f , Locale= locales.FirstOrDefault(l => l.Language == "en-UK") });
         TextToSpeech.Default.SpeakAsync($"{word.Arab}", new SpeechOptions { Pitch = 0f, Volume = 1f, Locale = locales.FirstOrDefault(l => l.Language == "ar-SA") });
-        TextToSpeech.Default.SpeakAsync($". In English. {word.Eng}", new SpeechOptions { Pitch = 0f, Volume = 1f, Locale = locales.FirstOrDefault(l => l.Language == "en-IE") });
+        TextToSpeech.Default.SpeakAsync($". In English. {word.Eng}", new SpeechOptions { Pitch = 0f, Volume = 1f, Locale = locales.FirstOrDefault(l => l.Language == "en-UK") });
     }
 }
--- TTSOffLine/MainPage.xaml.cs
+++ TTSOffLine/MainPage.xaml.cs
@@ -23,15 +23,15 @@ namespace TTSOffLine
                 {
                     Pitch = 0f,              // 0.0–1.0
                     Volume = 1f,             // 0.0–1.0
-                    Locale = locales.FirstOrDefault(l => l.Language == "en-IE")
+                    Locale = locales.FirstOrDefault(l => l.Language == "en-UK")
                 };
                 await TextToSpeech.Default.SpeakAsync(WordEntry.Text, options);
                 //TextToSpeech.Default.SpeakAsync(WordEntry.Text, new SpeechOptions { Pitch = 1, Volume = 1 }, CancellationToken.None);
             }
             catch (Exception ex)
             {
-                //DisplayAlert("Alert", ex.Message, "OK");
-                Toast.Make(ex.Message, ToastDuration.Long).Show();
+                DisplayAlert("Alert", ex.Message, "OK");
+                //Toast.Make(ex.Message, ToastDuration.Long).Show();
             }            
         }
         private void ImgTap_Tapped(object sender, TappedEventArgs e)
